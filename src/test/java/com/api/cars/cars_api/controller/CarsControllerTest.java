f6c9b947d78c50269c2e9cc6c7a8892ca20ed40d
DESKTOP-SP1RLBN\Johnny Medina <johnny21noberto@gmail.com>
2025-02-19 22:11:48 +0000

    Develop: creation of test classes without the implementation.

---
+++ src/test/java/com/api/cars/cars_api/controller/CarsControllerTest.java
@@ -0,0 +1,53 @@
+package com.api.cars.cars_api.controller;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+class CarsControllerTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void getAllCars() {
+    }
+
+    @Test
+    void getASpecificCarInfo() {
+    }
+
+    @Test
+    void getCarsByBrand() {
+    }
+
+    @Test
+    void getCarsByVersion() {
+    }
+
+    @Test
+    void createANewCar() {
+    }
+
+    @Test
+    void deleteACar() {
+    }
+
+    @Test
+    void deleteAllCars() {
+    }
+
+    @Test
+    void updateACar() {
+    }
+
+    @Test
+    void updateACarPartially() {
+    }
+}
